8801437d73c1ac3e02857e6d5032e05d3404b3c6
Ajin Saji <ajinsaji2004@gmail.com>
2024-11-14 21:32:01 +0530

    Add files via upload

---
+++ Day1/30 day of 1.py
@@ -0,0 +1,8 @@
+'''
+Author : Ajin Saji
+Description:The print() function prints the
+specified message to the screen or other standard output devices.
+The message can be a string or any other object; the object will be converted
+into a string before being displayed.
+'''
+print("I love Python")
