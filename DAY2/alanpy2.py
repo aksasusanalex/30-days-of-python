1f057f7a562ca2ae0107d8d912db60ba3b40a33b
ALANBINU0 <alanbinut@gmail.com>
2024-11-14 21:21:44 +0530

    alanpy2.py

--- DAY2/alanpy2.py
+++ DAY2/alanpy2.py
@@ -3,11 +3,9 @@ Author:Alan Binu
 Date:12-11-2024
 Description:"Write a Python Program that operates the following arithmetic operations"
 '''
-#Addition
-print(66+25)
-#Subtraction
-print(25-20)
-#Multiplication
-print(9*5)
-#Division
-print(55/11)
+p=int(input("enter your number")
+q=int(input("enter your number")
+print(p+q)
+print(p-q)
+print(p*q)
+print(p/q)
