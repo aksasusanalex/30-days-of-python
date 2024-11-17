63358b0dd7436cbd185b969ecc45abb572885558
Alda987 <aldatreesajosy05@gmail.com>
2024-11-17 21:00:04 +0100

    Add files via upload

---
+++ DAY2/alda2.py
@@ -0,0 +1,13 @@
+'''Author: ALDA TREESA JOSY 
+Date:17/11/24
+Description:Python allows you to perform basic mathematical operations such as addition, subtraction, multiplication, and division using operators from the user. The result can be displayed using the print() function'''
+
+
+
+
+n=int(input("enter the number 1"))
+m=int(input("enter the number 2"))
+print(m+n)
+print(m-n)
+print(m*n)
+print(m/n)
