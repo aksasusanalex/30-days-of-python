f9bb4ad09780aeb1af697032164ca5713f85f3ae
Alda987 <aldatreesajosy05@gmail.com>
2024-11-17 21:02:28 +0100

    Add files via upload

---
+++ DAY4/alda4.py
@@ -0,0 +1,18 @@
+'''Author: ALDA TREESA JOSY 
+Date:17/11/2024
+Description:Write a Python program that continuously asks the user for a number using the input() function. The program should add the numbers entered by the user and print the cumulative sum. The loop should stop when the user enters a negative number. When the loop stops, print the total sum of the positive numbers entered.'''
+
+
+
+
+cumulative_sum = 0
+
+while True:
+ 
+    number = float(input("Enter a number (negative number to stop):"))
+    if number < 0:
+        break 
+    
+    
+    cumulative_sum += number
+print(f"The total sum of positive numbers is: {cumulative_sum}")
