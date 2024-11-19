65fbccf2841363a890569ffebcb2d2dace364147
Davis Christy Robert <daviscr2006@gmail.com>
2024-11-19 21:34:05 +0530

    Add files via upload

---
+++ Day1/davis1.py
@@ -0,0 +1,11 @@
+"""
+Author:Davis Christy Robert
+date:19-11-2024
+Description: The print() function prints the specified message to the screen or other standard output devices.
+The message can be a string or any other object;
+the object will be converted into a string before being displayed.
+
+
+"""
+a="i love python"
+print(a)
