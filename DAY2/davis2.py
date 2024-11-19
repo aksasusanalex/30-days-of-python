1f2d3e887355279595ee62172a54a14305311f7d
Davis Christy Robert <daviscr2006@gmail.com>
2024-11-19 21:54:43 +0530

    Add files via upload

---
+++ DAY2/davis2.py
@@ -0,0 +1,13 @@
+"""
+Author:Davis Christy Robert
+Date:19-11-2024
+Description:Python allows you to perform basic mathematical operations such as addition,
+subtraction, multiplication, and division using operators from the user.
+The result can be displayed using the print() function.
+"""
+a=int(input("enter your first number:"))
+b=int(input("enter your second number:"))
+print("The value of a and b after addition is:",a+b)
+print("The value of a and b after subtraction is:",a-b)
+print("The value of a and b after muliplication:",a*b)
+print("The value of a and b after division:",a/b)
